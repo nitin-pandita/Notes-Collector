d7ad2dea53cbfb9a98c283091d3ebfb6b4b311da
nitin-pandita <nitin_207013@saitm.org>
2023-05-04 23:44:44 +0530

    Files uploaded

---
+++ index.js
@@ -0,0 +1,13 @@
+// const navbar = document.querySelector('.navbar');
+
+// let lastScrollTop = 0;
+
+// window.addEventListener('scroll', function() {
+//     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//     if (scrollTop > lastScrollTop) {
+//         navbar.classList.add('navbar-scrolled');
+//     } else {
+//         navbar.classList.remove('navbar-scrolled');
+//     }
+//     lastScrollTop = scrollTop;
+// });
